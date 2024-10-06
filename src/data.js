cce24501352ab1893800aa16be24728e0b9426f7
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-10-06 04:16:56 +0300

    ' ' Proper ' version '

--- src/data.js
+++ src/data.js
@@ -2,26 +2,32 @@ export default [
   {
     question: "What language React based on?",
     answer: "JavaScript",
+    id: 1,
   },
   {
     question: "What are the building blcks of React apps?",
     answer: "Components",
+    id: 2,
   },
   {
     question: "What' s the name of the syntax use to describe a UI in React?",
-    answer: "?",
+    answer: "JSX    ",
+    id: 3,
   },
   {
     question: "How to insert a data through parent to child components?",
     answer: "Props",
+    id: 4,
   },
   {
     question: "How to give components memory?",
     answer: "State",
+    id: 5,
   },
   {
     question:
       "What to call an inpt ' elemnt ' is completely synchronised to state?",
     answer: "Controlled ' elemnt '",
+    id: 6,
   },
 ];
